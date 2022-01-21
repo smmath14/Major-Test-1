d35f665eacdf610d10017c46b8d162a07f6045c0
Sourabh Mathur <73380583+smmath14@users.noreply.github.com>
2022-01-21 11:28:45 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,47 +1,36 @@
-#write a code to create python prgram for word gussesing game like hangman
+# Write a Python program to create a word guessing game kind of like Hangman. Program should do the following operations:
 
+# a) Program chooses random word from list of words.
 
+# b) Limited guesses before game ends.
 
+# c) Addresses repeats when guessing.
 
-from random import randint
-l=input("Enter space seperated list of words : ").split()
-n=int(input("Enter number of guesses : "))
-g=0
-w = l[randint(0,len(l)-1)]
-word=[[x,False] for x in w]
-while(g<n):
-    print("Guesses remaining : ",n-g)
-    for x in word:
-        if x[1]:
-            print(x[0],end="")
+# d) Displays progress after each guess
+
+import random
+
+words = ['american']
+random_word = random.choice(words)
+count = 0
+attempt_list = []
+
+while True:
+    guess = str(input('Guess a letter: ')).lower()
+
+    if guess not in attempt_list:
+        if guess in random_word:
+            print('Its in the word')
         else:
-            print(end="_")
-    print()
-    s=input("Enter your guess : ")
-    r=False
-    f=True
-    for i in range(len(word)):
-        if word[i][0]==s:
-            if word[i][1]==False:
-                word[i][1]=True
-                f=False
-            else:
-                r=True
+            count += 1
+            print('Not in word, attemps: {}'.format(count))
+
+            if count >= 5:
+                print('\nYou have reached max attempts')
+                print('Sorry, but hangman died! You lose')
                 break
-    if f:
-        print("Wrong Guess")
-        g+=1
     else:
-        print("Correct Guess")
-        g+=1
-    if r:
-        print("Already Entered, guess again")
-    for x in word:
-        if not x[1]:
-            break
-    else:
-        print("You have guessed the word")
-        break
-else:
-    print("OUT OF GUESSES")
-print("ANSWER : ",w)
+        print('You already guessed {}. Try again'.format(guess))
+        print(set(attempt_list))
+
+    attempt_list.append(guess)
